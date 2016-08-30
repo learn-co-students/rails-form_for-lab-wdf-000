abf477a5d11fa5acfa78dc1a1dcd3d5927c9b800
ozPop <ozziepopovas@gmail.com>
2016-08-30 11:59:10 -0400

    refactor school_class controller, add params method

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -15,7 +15,7 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_class_params(:title, :room_number))
     @school_class.save
-    redirect_to school_class_path(@school)
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
@@ -24,6 +24,12 @@ class SchoolClassesController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
   end
 
 end
